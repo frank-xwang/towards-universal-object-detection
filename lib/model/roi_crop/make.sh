7113298d35abf0558b00fe06d79cf91ec93416b2
XudongWang <xuw080@ucsd.edu>
2019-11-27 15:03:34 -0800

    Remove useless ymal files

--- lib/model/roi_crop/make.sh
+++ lib/model/roi_crop/make.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-#CUDA_PATH=/usr/local/cuda/
-
-cd src
-echo "Compiling my_lib kernels by nvcc..."
-nvcc -c -o roi_crop_cuda_kernel.cu.o roi_crop_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_50
-=======
 CUDA_PATH=/usr/local/cuda/
 
 cd src
 echo "Compiling my_lib kernels by nvcc..."
 nvcc -c -o roi_crop_cuda_kernel.cu.o roi_crop_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
->>>>>>> 793eeda709a4483589939795954491531204c768
 
 cd ../
 python build.py
